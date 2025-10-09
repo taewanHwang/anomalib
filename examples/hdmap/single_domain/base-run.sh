70c5bb91d8d2cf988a5129aee3d47cc5bb4ebec6
taewanHwang <michel4546@gmail.com>
2025-10-09 17:02:15 +0000

    fix: exp4N->focal 0.5로 수정

--- examples/hdmap/single_domain/base-run.sh
+++ examples/hdmap/single_domain/base-run.sh
@@ -49,7 +49,7 @@ if [ ! -f "$PYTHON_CMD" ]; then
 fi
 
 # 사용 가능한 GPU 리스트
-AVAILABLE_GPUS=(12 13 14 15)
+AVAILABLE_GPUS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
 
 # 기본 설정
 PYTHON_SCRIPT="$SCRIPT_DIR/base-training.py"
